424ef9f4fcd2e6afd882636931ed3b07cdf9c579
adarsh31000 <adarshadarsh3100@gmail.com>
2025-03-18 17:17:31 -0400

    Updated deploy.yml for build and deploy pipeline

--- MyAspNetApp/obj/Debug/net8.0/MyAspNetApp.AssemblyInfo.cs
+++ MyAspNetApp/obj/Debug/net8.0/MyAspNetApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAspNetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b22e71a40c4c16b0ee179de68485c8aefc96ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1b741b7ca2ccb025f6c2b2d4f75aa4989456bbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAspNetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAspNetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
